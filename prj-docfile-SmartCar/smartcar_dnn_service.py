861cf1ee006041d50c17ea6e182b36210673f1a7
박문기(Moon-Kee Bahk) <30949880+mkbahk@users.noreply.github.com>
2022-06-06 13:34:22 +0900

    Update smartcar_dnn_service.py

--- prj-docfile-SmartCar/smartcar_dnn_service.py
+++ prj-docfile-SmartCar/smartcar_dnn_service.py
@@ -37,7 +37,7 @@ def predict():
         x=pd.DataFrame.from_dict(params, orient='index').transpose()
 
         with graph.as_default():
-            model = load_model('/root/DockerLearning/SmartCar/smartcar_dnn_model.h5')
+            model = load_model('/root/DockerLearning/prj-docfile-SmartCar/smartcar_dnn_model.h5')
             data["prediction"] = str(model.predict(x).argmax())
             data["success"] = True
         #endwith
